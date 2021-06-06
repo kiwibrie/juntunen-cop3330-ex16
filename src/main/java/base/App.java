8791ec412ddc810eb6daefe5ad42da6c6ebc7832
kiwibrie <brianne0128@gmail.com>
2021-06-06 14:05:00 -0500

    working base

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -11,5 +11,26 @@ public class App {
 
     public static void main(String[] args) {
         App myApp = new App();
+
+        int age = myApp.promptAge();
+
+        String msg = myApp.checkAge(age);
+
+        myApp.printOutput(msg);
+    }
+
+    public void printOutput(String msg){
+        System.out.print(msg);
+    }
+
+    public String checkAge(int age){
+        return age > 15
+                ? "You are old enough to legally drive."
+                : "You are not old enough to legally drive.";
+    }
+
+    public int promptAge(){
+        System.out.print("What is your age? ");
+        return Integer.parseInt(in.nextLine());
     }
 }
